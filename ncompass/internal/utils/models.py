b2da845a18f0b476b64525e715cbfd4cd79c65e7
Aditya Rajagopal <aditya.rajagopal@ncompass.tech>
2024-01-15 03:22:21 +0000

    Fixed previous PR comments

--- ncompass/internal/utils/models.py
+++ ncompass/internal/utils/models.py
@@ -2,14 +2,15 @@ import torch
 import logging as pylog
 from transformers import pipeline
 import ncompass.internal.logging as nclog
+from transformers.pipelines.base import Pipeline
 
-def __torch_num_params__(model: torch.nn.Module):
-    return sum([torch.prod(torch.tensor(x.shape)) for x in model.parameters()])
+def __torch_num_params__(model: torch.nn.Module) -> int:
+    return sum([int(torch.prod(torch.tensor(x.shape))) for x in model.parameters()])
 
-def torch_model_size_in_parameters_M(model: torch.nn.Module):
+def torch_model_size_in_parameters_M(model: torch.nn.Module) -> float:
     return __torch_num_params__(model) / 1e6
 
-def get_model_hf_pipeline(model_name: str, device: int = 0, **kwargs):
+def get_model_hf_pipeline(model_name: str, device: int = 0, **kwargs) -> Pipeline:
     logger = pylog.getLogger(nclog.DETAILED)
     logger.info(f"Loading model {model_name}")
     pipe = pipeline(model=model_name, device=device, **kwargs)
--- ncompass/models/snn/spike_gpt/tokenizer.py
+++ ncompass/models/snn/spike_gpt/tokenizer.py
@@ -1,6 +1,7 @@
 import json
 import torch
 import numpy as np
+from typing import Optional 
 from torch.nn import functional as F
 
 import ncompass.internal.logging as nclog
@@ -8,7 +9,7 @@ from ncompass.internal.models import NCTokenizer
 
 # === Imported code from ridgerchu/SpikeGPT (src/utils.py)
 class SpikeGPTTokenizer(NCTokenizer):
-    def __init__(self, word_name, unknown_char='\ue083'):
+    def __init__(self, word_name: str, unknown_char: str = '\ue083') -> None:
         if 'list' in str(type(word_name)):
             self.charMode = False
             if word_name[0] == word_name[1]:
@@ -30,17 +31,24 @@ class SpikeGPTTokenizer(NCTokenizer):
 
             self.unknown_char = self.stoi[unknown_char]
 
-    def refine_context(self, context):
-        context = context.strip().split('\n')
-        for c in range(len(context)):
-            context[c] = context[c].strip().strip('\u3000').strip('\r')
-        context = list(filter(lambda c: c != '', context))
-        context = '\n' + ('\n'.join(context)).strip()
+    def refine_context(self, ctxt: str) -> str:
+        context_list = ctxt.strip().split('\n')
+        for c in range(len(context_list)):
+            context_list[c] = context_list[c].strip().strip('\u3000').strip('\r')
+        context_list_filtered = list(filter(lambda c: c != '', context_list))
+        context = '\n' + ('\n'.join(context_list_filtered)).strip()
         if context == '':
             context = '\n'
         return context
 
-    def sample_logits(self, out, x, ctx_len, temperature=1.0, top_p_usual=None, top_p_newline=None):
+    def sample_logits(
+              self
+            , out: torch.Tensor
+            , x: torch.Tensor
+            , ctx_len: int
+            , temperature: float = 1.0
+            , top_p_usual: Optional[float] = None
+            , top_p_newline: Optional[float] = None) -> torch.Tensor:
         lastChar = int(x[-1])
 
         probs = F.softmax(torch.tensor(out), dim=-1)
