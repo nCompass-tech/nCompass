d71535545192fd3002c875e20a070fa0b2f4a192
Aditya Rajagopal <aditya.rajagopal@ncompass.tech>
2024-01-15 04:50:02 +0000

    Inference model loading + examples directory

---
+++ examples/spikegpt_inference.py
@@ -0,0 +1,29 @@
+import ncompass as nc
+
+def test_inference():
+    dataset = nc.loaders.huggingface.get_wikitext2("test") 
+    pipeline = nc.models.ann.gpt2.get_hf_pipeline(size="medium")
+    
+    tokenizer_json_path = "~/nCompass/ncompass/models/snn/spike_gpt/20B_tokenizer.json"
+    model_path = nc.models.download_from_hf(\
+                      repo_id = "ridger/SpikeGPT-OpenWebText-216M"\
+                    , filename = "SpikeGPT-216M.pth")
+    sgpt_config = nc.models.snn.spike_gpt.SpikeGPTConfig(\
+                      token_mode = "pile"\
+                    , word_name = [tokenizer_json_path, tokenizer_json_path]\
+                    , unknown_char = None\
+                    , vocab_size = 50277\
+                    , hidden_size = 768\
+                    , num_hidden_layers = 18\
+                    , ctx_len = 1024\
+                    , tokenizer_class = 'SpikeGPT'\
+                    , temperature = 1.5\
+                    , top_p = 0.7\
+                    , name_or_path = model_path)
+    tokenizer = nc.models.get_tokenizer(sgpt_config)
+    encodings = nc.loaders.huggingface.get_encodings(dataset, tokenizer)
+    
+    inference_model = nc.models.snn.spike_gpt.inference.SpikeGPT(sgpt_config)
+
+if __name__ == '__main__':
+    test_inference()
