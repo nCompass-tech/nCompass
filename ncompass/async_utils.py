ed3568486ffa6d7c8f7b982cdf60cd67b120b5b4
Aditya Rajagopal <adityarajagopal@ncompass.tech>
2024-02-21 19:26:00 -0800

    Created first pip package

--- ncompass/async_utils.py
+++ ncompass/async_utils.py
@@ -38,25 +38,3 @@ async def async_post(url, headers, body, stream=True, verify=False):
             else:
                 res = await response.json()
                 error_msg(res['error'])
-
-def build_body_from_params(prompt
-                           , miid
-                           , max_tokens = 1
-                           , temperature = 0
-                           , top_p = 0.50
-                           , stream = True):
-    body = {'prompt':        prompt
-            , 'miid':        miid
-            , 'max_tokens':  max_tokens
-            , 'temperature': temperature
-            , 'top_p':       top_p
-            , 'stream':      stream}
-    return body
-
-def stream(url: str
-           , miid: str
-           , prompt: str
-           , params: dict
-           , headers: dict):
-    body = build_body_from_params(prompt, miid, **params) 
-    return get_sync_generator_from_async(async_post(url, headers, body))
