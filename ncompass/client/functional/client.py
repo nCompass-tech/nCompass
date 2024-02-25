575867de9667889060a0ac8cc80ea4648647ab81
Aditya Rajagopal <aditya.rajagopal@ncompass.tech>
2024-02-25 00:47:36 -0800

    Added smarter error handling of non-live models / timeout if gpuserver is down

--- ncompass/client/functional/client.py
+++ ncompass/client/functional/client.py
@@ -1,4 +1,6 @@
 import time
+from datetime import datetime, timedelta
+
 from .run_prompt import stream_prompt
 from ncompass.network_utils import get
 from .model_health import check_model_health
@@ -12,11 +14,22 @@ def stop_model(url, api_key):
     return get(f'{url}/stop_model', {'Authorization': api_key})
 
 def model_is_running(url, api_key):
-    return check_model_health(url, api_key).status_code == 200
+    response = check_model_health(url, api_key)
+    if (response.status_code == 404) or (response.status_code == 400):
+        raise RuntimeError(response.text) 
+    elif response.status_code == 200:
+        return True
+    else:
+        return False
 
 def wait_until_model_running(url, api_key):
-    while not model_is_running(url, api_key):
-        continue
+    break_loop = False
+    wait_until = datetime.now() + timedelta(seconds=10)
+    while not break_loop:
+        if model_is_running(url, api_key): break_loop = True
+        if wait_until < datetime.now(): 
+            raise RuntimeError(f'TIMEOUT: Model {api_key} has not launched. ' 
+                                'Please try again in some time or contact admin@ncompass.tech')
 
 def complete_prompt(url
                     , api_key
