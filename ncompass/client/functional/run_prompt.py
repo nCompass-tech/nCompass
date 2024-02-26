3411f0837436edaf899def41ae097d4a97e4e6cd
Aditya Rajagopal <aditya.rajagopal@ncompass.tech>
2024-02-25 18:35:20 -0800

    Reverted prompt stream reading loop to be monolithic as functional decomp was causing aiohttp client session issues

--- ncompass/client/functional/run_prompt.py
+++ ncompass/client/functional/run_prompt.py
@@ -1,8 +1,8 @@
 import json
+import aiohttp
 import asyncio
 
 from ncompass.errors import error_msg
-from ncompass.network_utils import async_streaming_post_json
 from ncompass.async_utils import get_sync_generator_from_async
 
 def build_body_from_params(prompt
@@ -19,29 +19,29 @@ def build_body_from_params(prompt
             , 'stream':      stream}
     return body
 
-async def response_generator(response):
-    response_len = 0
-    async for chunk, _ in response.content.iter_chunks():
-        try:
-            split_chunk = chunk.split(b'\0')
-            decoded_chunk = split_chunk[0].decode('utf-8')
-            try:
-                data = json.loads(decoded_chunk)
-                res = data['text'][0] 
-                new_data = res[response_len:]
-                response_len = len(res)
-                yield new_data
-            except Exception as e:
-                yield ''
-        except Exception as e:
-            error_msg(str(e))
-
-async def stream_response_handler(response):
-    if response.status == 200:
-       return response_generator(response)
-    else:
-        res = await response.json()
-        error_msg(res['error'])
+async def get_streamed_response(url, payload, headers):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'https://{url}', headers=headers, json=payload, ssl=False)\
+                as response:
+            if response.status == 200:
+                response_len = 0
+                async for chunk, _ in response.content.iter_chunks():
+                    try:
+                        split_chunk = chunk.split(b'\0')
+                        decoded_chunk = split_chunk[0].decode('utf-8')
+                        try:
+                            data = json.loads(decoded_chunk)
+                            res = data['text'][0] 
+                            new_data = res[response_len:]
+                            response_len = len(res)
+                            yield new_data
+                        except Exception as e:
+                            yield ''
+                    except Exception as e:
+                        error_msg(str(e))
+            else:
+                res = await response.json()
+                error_msg(res['error'])
 
 def stream_prompt(url: str
                   , miid: str
@@ -54,9 +54,6 @@ def stream_prompt(url: str
     _url = f'{url}/run_prompt'
     headers = {'Content-Type': "application/json", 'Authorization': miid} 
     body = build_body_from_params(prompt, miid, max_tokens, temperature, top_p, stream) 
-    loop = asyncio.get_event_loop()
-    res = loop.run_until_complete(async_streaming_post_json(_url
-                                                            , body
-                                                            , headers
-                                                            , stream_response_handler))
-    return get_sync_generator_from_async(res)
+    return get_sync_generator_from_async(get_streamed_response(_url
+                                                               , body
+                                                               , headers))
--- ncompass/network_utils.py
+++ ncompass/network_utils.py
@@ -10,9 +10,3 @@ def post_json(url, payload, headers=None):
     if headers is not None: hdr.update(headers)
     client = httpx.Client(timeout=20, verify=False)
     return client.post(f'https://{url}', headers=hdr, json=payload)
-
-async def async_streaming_post_json(url, payload, headers, handler):
-    session = aiohttp.ClientSession()
-    response = await session.post(f'https://{url}', headers=headers, json=payload, ssl=False)
-    return (await handler(response))
-
