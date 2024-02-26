f87225d21c8a5887cd229ddce17c0ee748474d7b
Aditya Rajagopal <aditya.rajagopal@ncompass.tech>
2024-02-26 13:42:26 -0800

    cli fixes

--- ncompass/cli/start_session.py
+++ ncompass/cli/start_session.py
@@ -3,8 +3,7 @@ import ncompass.client.functional as F
 from ncompass.network_utils import exec_url
 
 def start_session(api_key):
-    response = F.start_session(exec_url(), api_key) 
-    print(response.text, response.status_code)
+    F.start_session(exec_url(), api_key) 
 
 def main():
     start_session(sys.argv[1])
