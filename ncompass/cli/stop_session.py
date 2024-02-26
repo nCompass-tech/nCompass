66c84f52d71fab45bf13092b8e1e13afb8f96844
adirajagopal <151404949+adirajagopal@users.noreply.github.com>
2024-02-26 13:54:26 -0800

    cli fixes (#14)

--- ncompass/cli/stop_session.py
+++ ncompass/cli/stop_session.py
@@ -3,8 +3,7 @@ import ncompass.client.functional as F
 from ncompass.network_utils import exec_url
 
 def stop_session(api_key):
-    response = F.stop_session(exec_url(), api_key)
-    print(response.text, response.status_code)
+    F.stop_session(exec_url(), api_key)
 
 def main():
     stop_session(sys.argv[1])
